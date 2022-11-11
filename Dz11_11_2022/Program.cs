9a8e8b9030cb60c84a6e30511d51f933239af19f
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-11-11 13:29:10 +0300

    Dz1

--- Dz11_11_2022/Program.cs
+++ Dz11_11_2022/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+int check = 5;
+int[] mass = new int[] { };
+check = dz1_1(mass);
+Console.WriteLine(check);
+
+static int dz1_1(int[] intMassiv)
+{
+    if (intMassiv.Length == 0) return -1; // пустой масив
+    if (intMassiv.Length >= 10) return -2;// слишком большой
+    if (intMassiv.Length <= 3) return -3; // слишком маленький
+    return 0;
+}
