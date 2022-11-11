5ef78ffbd4045f7e08ab47b44a3dc35aca4cd7f8
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-11-11 14:12:58 +0300

    dz3

--- Dz11_11_2022/Program.cs
+++ Dz11_11_2022/Program.cs
@@ -19,3 +19,29 @@ static int dz1_1(int[] intMassiv)
 // неверный формат при попытки первести сифвол в число,
 // пустой двухмерный масив масив,
 // в двухмерном масиве буду встречаться тексты на не символы
+
+int[] masiv1 = new int[] { 10, 15, 30, 70, 5 };
+int[] masiv2 = new int[] { 5, 10, 15, 35, 2 };
+int[] masiv3 = dz3(masiv1, masiv2);
+printer(masiv3);
+
+static int[] dz3(int[] masiv1, int[] masiv2)
+{
+    if (masiv1.Length != masiv2.Length) throw new Exception("Масивы не равны");
+
+    int[] masiv3 = new int[masiv1.Length];
+    for (int i = 0; i < masiv1.Length; i++)
+    {
+        masiv3[i] = masiv1[i] - masiv2[i];
+
+    }
+    return masiv3;
+}
+
+static void printer(int[] arry)
+{
+    foreach (var item in arry)
+    {
+        Console.WriteLine(item);
+    }
+}
